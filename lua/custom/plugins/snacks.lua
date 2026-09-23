182302be28df6b1a6047774943af87d8c32201f3
Brad Gaddis <bradleygad@protonmail.com>
2026-09-22 20:58:11 -0400

    restore curated snacks picker: re-enable picker module + non-colliding keys, keep telescope ui.select

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -13,6 +13,7 @@ return {
     indent = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
+    picker = { enabled = true, ui_select = false }, -- telescope-ui-select owns vim.ui.select
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
@@ -21,12 +22,85 @@ return {
     words = { enabled = true },
   },
   keys = {
+    -- Pickers (only keys that don't clash with telescope/conform)
+    { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
+    { '<leader>fc', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, desc = 'Find Config File' },
+    { '<leader>ff', function() Snacks.picker.files() end, desc = 'Find Files' },
+    { '<leader>fg', function() Snacks.picker.git_files() end, desc = 'Find Git Files' },
+    {
+      '<leader>fp',
+      function()
+        Snacks.picker.projects { dev = { '~/Projects/', '~/Projects/Godot_Projects/' }, max_depth = 4, matcher = { frecency = true } }
+      end,
+      desc = 'Projects',
+    },
+    { '<leader>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
+    -- git
+    { '<leader>gb', function() Snacks.picker.git_branches() end, desc = 'Git Branches' },
+    { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
+    { '<leader>gL', function() Snacks.picker.git_log_line() end, desc = 'Git Log Line' },
+    { '<leader>gs', function() Snacks.picker.git_status() end, desc = 'Git Status' },
+    { '<leader>gS', function() Snacks.picker.git_stash() end, desc = 'Git Stash' },
+    { '<leader>gd', function() Snacks.picker.git_diff() end, desc = 'Git Diff (Hunks)' },
+    { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
+    -- gh
+    { '<leader>gi', function() Snacks.picker.gh_issue() end, desc = 'GitHub Issues (open)' },
+    { '<leader>gI', function() Snacks.picker.gh_issue { state = 'all' } end, desc = 'GitHub Issues (all)' },
+    { '<leader>gp', function() Snacks.picker.gh_pr() end, desc = 'GitHub Pull Requests (open)' },
+    { '<leader>gP', function() Snacks.picker.gh_pr { state = 'all' } end, desc = 'GitHub Pull Requests (all)' },
+    -- grep (sg/sw/sd/sh/sk/ss/sc/s/ live on telescope)
+    { '<leader>sb', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
+    { '<leader>sB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
+    -- search
+    { '<leader>s"', function() Snacks.picker.registers() end, desc = 'Registers' },
+    { '<leader>sa', function() Snacks.picker.autocmds() end, desc = 'Autocmds' },
+    { '<leader>sC', function() Snacks.picker.commands() end, desc = 'Commands' },
+    { '<leader>sD', function() Snacks.picker.diagnostics_buffer() end, desc = 'Buffer Diagnostics' },
+    { '<leader>sH', function() Snacks.picker.highlights() end, desc = 'Highlights' },
+    { '<leader>si', function() Snacks.picker.icons() end, desc = 'Icons' },
+    { '<leader>sj', function() Snacks.picker.jumps() end, desc = 'Jumps' },
+    { '<leader>sl', function() Snacks.picker.loclist() end, desc = 'Location List' },
+    { '<leader>sm', function() Snacks.picker.marks() end, desc = 'Marks' },
+    {
+      '<leader>sM',
+      function()
+        Snacks.picker.man {
+          finder = 'system_man',
+          format = 'man',
+          preview = 'man',
+          confirm = function(picker, item, action)
+            ---@cast action snacks.picker.jump.Action
+            picker:close()
+            if item then
+              vim.schedule(function()
+                local cmd = 'Man ' .. item.ref ---@type string
+                if action.cmd == 'vsplit' then
+                  cmd = 'vert ' .. cmd
+                elseif action.cmd == 'tab' then
+                  cmd = 'tab ' .. cmd
+                end
+                vim.cmd(cmd)
+              end)
+            end
+          end,
+        }
+      end,
+      desc = 'Man Pages',
+    },
+    { '<leader>sp', function() Snacks.picker.lazy() end, desc = 'Search for Plugin Spec' },
+    { '<leader>sq', function() Snacks.picker.qflist() end, desc = 'Quickfix List' },
+    { '<leader>sR', function() Snacks.picker.resume() end, desc = 'Resume' },
+    { '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
+    { '<leader>su', function() Snacks.picker.undo() end, desc = 'Undo History' },
+    { '<leader>uC', function() Snacks.picker.colorschemes() end, desc = 'Colorschemes' },
     -- Other
     { '<leader>z', function() Snacks.zen() end, desc = 'Toggle Zen Mode' },
     { '<leader>Z', function() Snacks.zen.zoom() end, desc = 'Toggle Zoom' },
     { '<leader>.', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer' },
+    { '<leader>S', function() Snacks.scratch.select() end, desc = 'Select Scratch Buffer' },
     { '<leader>n', function() Snacks.notifier.show_history() end, desc = 'Notification History' },
     { '<leader>bd', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
+    { '<leader>cR', function() Snacks.rename.rename_file() end, desc = 'Rename File' },
     {
       '<leader>gB',
       function()
